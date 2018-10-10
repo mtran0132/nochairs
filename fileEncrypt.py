f24fef65b4b6aad2883b03358118267672882d7f
Marco Tran <mtran0132@gmail.com>
2018-10-09 23:43:53 -0700

    encrypt and decrypt functions

--- fileEncrypt.py
+++ fileEncrypt.py
@@ -1,36 +1,91 @@
-import os
+#https://www.reddit.com/r/learnpython/comments/51hxul/how_do_you_split_a_binary_file_into_specific/
+
+import os, base64
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 
-CONST_BIT_LENGTH = 128
-CONST_IV = os.urandom(16)
-CONST_BACKEND = default_backend()
+CONST_KEY_BYTES = 32
+CONST_IV_BYTES = 16
+CONST_FILE_PATH = "picture.jpeg"
 
 def main():
+	# Creates a random string of 32 bytes
+	key = os.urandom(CONST_KEY_BYTES)
+	message = b"This is a secret message. ATTACK AT DAWN FROM THE NORTH."
+	(cipherText, IV) = myEncrypt(message, key)
+	print("Cipher Text: ", cipherText)
+	plainText = myDecrypt(cipherText, key, IV)
+	print("Plain Text: ", plainText)
 
 
-
+# Encrypts a message using a random key generated from the OS
 def myEncrypt(message, key):
 
-def myDecrypt(message, key):
+	if(len(key) < CONST_KEY_BYTES):
+		print("Error: key length less than 32 bytes")
+		return
+		
+	# Creates a random string of 16 bytes
+	iv = os.urandom(CONST_IV_BYTES)
 
-def myFileEncrypt(filepath):
+	# Here we set the parameters for the encryptor
+	# We use AES and CBC
+	encryptor = Cipher(algorithms.AES(key), modes.CBC(iv), backend = default_backend()).encryptor()
 
-def myFileDecrypt(filepath, key):
+	# Encode the message to base64
+	encoded = base64.b64encode(message)
+
+	# TAKEN FROM https://stackoverflow.com/questions/14179784/python-encrypting-with-pycrypto-aes/14205319#14205319
+	# AES requires plain text and ciphertext to be a multiple of 16
+	# We pad it so that the message is a multiple of the IV, 16
+	length = CONST_IV_BYTES - (len(encoded) % CONST_IV_BYTES)
+	encoded += bytes([length])*length
+
+	# We encrypt the message
+	cipherText = encryptor.update(encoded) + encryptor.finalize()
+
+	return (cipherText, iv)
+
+# Decrypts a message using returned parameters from encrypt function 
+def myDecrypt(cipherText, key, iv):
+	if(len(key) < CONST_KEY_BYTES):
+		print("Error: key length less than 32 bytes")
+		return
+
+	# Here we set the parameters for the decryptor	
+	decryptor = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend()).decryptor()
+
+	# The cipherText gets decrypted but it is still encoded
+	encoded = decryptor.update(cipherText) + decryptor.finalize()
+
+	# TAKEN FROM https://stackoverflow.com/questions/14179784/python-encrypting-with-pycrypto-aes/14205319#14205319
+	# Here we remove the padding
+	decoded = encoded[:-encoded[-1]]
+
+	# We decode from base64
+	plainText = base64.b64decode(decoded)
+
+	# Return the plaintext
+	return plainText
+
+
+# def myFileEncrypt(filepath):
+
+# def myFileDecrypt(filepath, key):
 
 
 main()
 
 
-backend = default_backend()
-key = os.urandom(32)
-iv = os.urandom(16)
-cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
-encryptor = cipher.encryptor()
-ct = encryptor.update(b"a secret message") + encryptor.finalize()
-decryptor = cipher.decryptor()
-decryptor.update(ct) + decryptor.finalize()
-b'a secret message'
+# backend = default_backend()
+# key = os.urandom(32)
+# iv = os.urandom(16)
+# cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
+# encryptor = cipher.encryptor()
+# ct = encryptor.update(b"a secret message") + encryptor.finalize()
+# decryptor = cipher.decryptor()
+# decryptor.update(ct) + decryptor.finalize()
+# b'a secret message'
 
 
 # (C, IV)= Myencrypt(message, key):
