9d2777d77a805e71812eb5ca3c403fc7353e87dd
mtran0132 <mtran0132@gmail.com>
2018-10-10 13:27:56 -0700

    fixed padding functions, removed filecheck function

--- fileEncrypt.py
+++ fileEncrypt.py
@@ -2,19 +2,13 @@
 # Will implement a module that saves the key and iv
 # So that you can encrypt the file, exit the program, then run it again to decrypt it.
 
-import os, base64
+import os, base64, constants
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
-
-CONST_KEY_BYTES = 32
-CONST_IV_BYTES = 16
-CONST_FILE_PATH = "picture.jpg"
-CWD_PATH = os.getcwd()
-
-
+from cryptography.hazmat.primitives import padding
 def main():
 	# Creates a random string of 32 bytes
-	key = os.urandom(CONST_KEY_BYTES)
+	key = os.urandom(constants.CONST_KEY_BYTES)
 	message = b"This is a secret message. ATTACK AT DAWN FROM THE NORTH."
 	print("Original Message: ", message,"\n")
 	(cipherText, IV) = myEncrypt(message, key)
@@ -29,27 +23,28 @@ def main():
 
 	fileChoice = input("Which file to encrypt? Otherwise, 'exit' to exit: ")
 	while(fileChoice != "exit"):
-		# Makes an absolute path to the file
-		inFile = os.path.join(CWD_PATH,fileChoice)
-		if(fileCheck(inFile)):
+
+		if(os.path.isfile(fileChoice)):
+
 			(fileCipherText, fileIv, fileKey, fileExt) = myFileEncrypt(fileChoice)
 			dummy_text = input("Check the file to see if it is encrypted!\nThen press enter\n")
 			myFileDecrypt(fileChoice, fileKey, fileIv)
 			fileChoice = input("Which file to encrypt? Otherwise, 'exit' to exit: ")
 
 		else:
+			print("File doesn't exist, try again")
 			fileChoice = input("Which file to encrypt? Otherwise, 'exit' to exit: ")
 
 
 # Encrypts a message using a random key generated from the OS
 def myEncrypt(message, key):
 
-	if(len(key) < CONST_KEY_BYTES):
+	if(len(key) < constants.CONST_KEY_BYTES):
 		print("Error: key length less than 32 bytes")
 		return
 		
 	# Creates a random string of 16 bytes
-	iv = os.urandom(CONST_IV_BYTES)
+	iv = os.urandom(constants.CONST_IV_BYTES)
 
 	# Here we set the parameters for the encryptor
 	# We use AES and CBC
@@ -65,7 +60,7 @@ def myEncrypt(message, key):
 
 # Decrypts a message using returned parameters from encrypt function 
 def myDecrypt(cipherText, key, iv):
-	if(len(key) < CONST_KEY_BYTES):
+	if(len(key) < constants.CONST_KEY_BYTES):
 		print("Error: key length less than 32 bytes")
 		return
 
@@ -88,20 +83,17 @@ def myDecrypt(cipherText, key, iv):
 # Given a file within the same working directory it will encrypt it
 def myFileEncrypt(fileName):
 
-	# Makes an absolute path to the file
-	inFile = os.path.join(CWD_PATH,fileName)
-
 	# Checks if file exists
-	if(fileCheck(inFile)):
+	if(os.path.isfile(fileName)):
 	
 		# Makes a 32 byte key
-		key = os.urandom(CONST_KEY_BYTES)
+		key = os.urandom(constants.CONST_KEY_BYTES)
 		
 		# Splits the file name and file extension
-		fileName, fileExt = os.path.splitext(fileName)
+		fileLabel, fileExt = os.path.splitext(fileName)
 		
 		# Opens the file to read it as binary
-		with open(inFile, "rb") as file:
+		with open(fileName, "rb") as file:
 			file_string = file.read()
 
 			# Encrypts the file_string using the key
@@ -109,7 +101,7 @@ def myFileEncrypt(fileName):
 			cipherFile, iv = myEncrypt(file_string, key)
 		
 		# Opens the file to overwrite it as binary
-		with open(inFile, "wb") as file:
+		with open(fileName, "wb") as file:
 
 			# Overwrites whatever is inside with the encrypted encoded cipherFile
 			file.write(cipherFile)
@@ -122,14 +114,11 @@ def myFileEncrypt(fileName):
 # Given the same file you want to decrypt and the same key and IV used to encrypt
 def myFileDecrypt(fileName, key, iv):
 
-	# Make an absolute path to the file
-	inFile = os.path.join(CWD_PATH,fileName)
-
 	# Checks if file exists
-	if(fileCheck(inFile)):
+	if(os.path.isfile(fileName)):
 
 		# Opens the file to read it as binary
-		with open(inFile, "rb") as file:
+		with open(fileName, "rb") as file:
 
 			# Reads the encrypted encoded file
 			file_string = file.read()
@@ -138,7 +127,7 @@ def myFileDecrypt(fileName, key, iv):
 			plainTextFile = myDecrypt(file_string,key,iv)
 		
 		# Opens the file to overwrite it as binary
-		with open(inFile, "wb") as file:
+		with open(fileName, "wb") as file:
 			file.write(plainTextFile)
 			file.close()
 
@@ -146,44 +135,29 @@ def myFileDecrypt(fileName, key, iv):
 # AES requires plain text and ciphertext to be a multiple of 16
 # We pad it so that the message is a multiple of the IV, 16
 def addPadding(encoded):
-	# TAKEN FROM https://stackoverflow.com/questions/14179784/python-encrypting-with-pycrypto-aes/14205319#14205319
-	length = CONST_IV_BYTES - (len(encoded) % CONST_IV_BYTES)
-	encoded += bytes([length])*length
-	return encoded
+	
+	# We pad it with 128 bits or 16 bytes
+	padder = padding.PKCS7(128).padder()
+
+	# update() pads the encoded message
+	padded_encoded = padder.update(encoded)
+
+	# .finalize () Returns the remainder of the data.
+	padded_encoded += padder.finalize()
+	return padded_encoded
 
 
 # Here we remove the padding
-def removePadding(encoded):
-	# TAKEN FROM https://stackoverflow.com/questions/14179784/python-encrypting-with-pycrypto-aes/14205319#14205319
-	encoded = encoded[:-encoded[-1]]
-	
-	return encoded
+def removePadding(padded_encoded):
 
-# FileCheck returns 'True' or 'False' if the file exists 
-# This is a safeguard against non-existant files
-def fileCheck(file):
-	try:
-		open(file, "r")
-		return True
-	except IOError:
-		print("Can't find file: \n" + file + "\nTry again...\n")
-		return False
+	# We unpad it using 128 bits or 16 bytes
+	unpadder = padding.PKCS7(128).unpadder()
 
+	# update() unpads the padded encoded message
+	unpadded_encoded = unpadder.update(padded_encoded)
+
+	# finalize() Returns the remainder of the data.
+	unpadded_encoded += unpadder.finalize()
+	return unpadded_encoded
 
 main()
-
-
-
-
-# (C, IV)= Myencrypt(message, key):
-
-# In this method, you will generate a 16 Bytes IV, and encrypt the message using the key and IV in CBC mode (AES).  
-# You return an error if the len(key) < 32 (i.e., the key has to be 32 bytes= 256 bits).
-
-# (C, IV, key, ext)= MyfileEncrypt (fileName):
-
-# In this method, you'll generate a 32Byte key. You open and read the file as a string. 
-# You then call the above method to encrypt your file using the key you generated. 
-# You return the cipher C, IV, key and the extension of the file (as a string).
-
-# You'll have to write the inverse of the above methods. 
